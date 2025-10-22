3887c7a3c867db27173e4fdf08691d92dc9ad2e2
StarStorms21 <theopitsiloglou@gmail.com>
2025-10-22 20:39:45 +0300

    successful run

--- src/main/java/com/github/theopitsihue/stise_springroll/entity/User.java
+++ src/main/java/com/github/theopitsihue/stise_springroll/entity/User.java
@@ -1,4 +1,22 @@
 package com.github.theopitsihue.stise_springroll.entity;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
+    @Id //defines this field as the primary key
+    private Long id;
+    private String username;
+    private String password;
+    private String emailID;
 }
