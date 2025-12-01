4d239fdb85936953cb0e77f1152358dc630c1e4d
pi3sl1ce <57503652+pi3sl1ce@users.noreply.github.com>
2025-12-01 23:16:40 +0200

    feat(login): add form fields and conditional signup fields

--- Springroll-Client/src/components/Login/Login.jsx
+++ Springroll-Client/src/components/Login/Login.jsx
@@ -4,8 +4,32 @@ const Login = () => {
   const [isLoginMode, setIsLoginMode] = useState(true);
 
   return (
-    <div>
+     <div>
       <h2>{isLoginMode ? "Login" : "Sign Up"}</h2>
+      
+      {/* Toggle Buttons */}
+      <div>
+        <button onClick={() => setIsLoginMode(true)}>
+            Login
+        </button>
+        <button onClick={() => setIsLoginMode(false)}>
+            Sign Up
+        </button>
+      </div>
+
+      {/* Form Fields */}
+      <form>
+        {!isLoginMode && 
+            <input placeholder="Name" />}
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+        {!isLoginMode && 
+            <input type="password" placeholder="Confirm Password" />}
+
+        <button type="submit">
+            {isLoginMode ? "Login" : "Sign Up"}
+        </button>
+      </form>
     </div>
   );
 }
