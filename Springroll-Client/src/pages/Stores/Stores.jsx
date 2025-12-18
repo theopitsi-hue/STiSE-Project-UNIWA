720ba175328cd990124a2bff786a58ef1a7b0cb3
pi3sl1ce <57503652+pi3sl1ce@users.noreply.github.com>
2025-12-18 14:35:09 +0200

    feat(stores): add loading and stores state

--- Springroll-Client/src/pages/Stores/Stores.jsx
+++ Springroll-Client/src/pages/Stores/Stores.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState} from 'react'
 
 const Stores = () => {
+  const [stores, setStores] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   return (
     <div className="p-6 sm:p-8 bg-[#0f0f0f] min-h-screen">
       <h1 className="text-3xl font-bold mb-10 text-springOrange text-center">
